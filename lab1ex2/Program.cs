70291ca5490f7d50b7fa7a027e4ef8949c151fd0
uimkatali <b8bf6b5@gmail.com>
2023-09-17 20:56:52 +0300

    Add project files.

---
+++ lab1ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+/*
+Scrieti un program care va calcula media aritmetica a trei numere citite de la
+tastatura */
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a=Convert.ToInt32(Console.ReadLine());
+            b=Convert.ToInt32(Console.ReadLine());
+            c=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine((a+b+c)/3);
+        }
+    }
+}
